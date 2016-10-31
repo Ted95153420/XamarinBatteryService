848cf516f26017ea64ef2de9d37ce6cbb3e540a7
Ted Milne <edard.milne@googlemail.com>
2016-10-31 08:52:56 -0700

    StartupBroadCastreciever added to project

--- BatteryService/AndroidBatteryService/BatteryBroadCastReciever.cs
+++ BatteryService/AndroidBatteryService/BatteryBroadCastReciever.cs
@@ -11,6 +11,9 @@ namespace AndroidBatteryService
     {
         public override void OnReceive(Context context, Intent intent)
         {
+            //Here is where you do "Something" when the battery level changes.
+            //As an example I assigned the battery level to an Integer. You can put code in here to do anything
+            //(i.e - report battery level to your Web App.) If you would like help with this, let me know...
             int level = intent.GetIntExtra("level", 0);
         }
     }
--- BatteryService/AndroidBatteryService/StartupBoadcastReciever.cs
+++ BatteryService/AndroidBatteryService/StartupBoadcastReciever.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Content;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace AndroidBatteryService
 {
-    class StartupBoadcastReciever
+    //The sole purpose of this class is to start the Android Battery Service
+    //when the phone starts.
+    public class StartupBoadcastReciever : BroadcastReceiver
     {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Intent serviceStrtIntent = new Intent(Android.App.Application.Context, typeof(AndroidBatteryService));
+            serviceStrtIntent.SetFlags(ActivityFlags.NewTask);
+            Android.App.Application.Context.StartService(serviceStrtIntent);
+        }
     }
 }
